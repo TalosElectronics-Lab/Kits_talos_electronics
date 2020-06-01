69d16f8cbd4e15584a5f446fc63abdb22b29f8b7
Rafa <rafa.lozano.rolon@gmail.com>
2020-06-01 16:09:06 -0500

    robot 3 en 1 nueva funcion *Evasor de obstaculos

--- Kit_3_en_1.cpp
+++ Kit_3_en_1.cpp
@@ -4,6 +4,12 @@ void Kit_3_en_1::init()
     Motores_init();
     Bluetooth.begin(9600);
     Serial.begin(9600);
+
+    Motores_init();
+    pinMode(Trigger, OUTPUT);
+    pinMode(Echo, INPUT);
+    servo_1.attach(Servo_pin);
+    servo_1.write(90);
 }
 void Kit_3_en_1::modo_3_en_1(){
     Serial.print("Estado: ");
@@ -79,7 +85,7 @@ void Kit_3_en_1::modo_3_en_1(){
         break;
 
     case 20:
-        //Robot.Modo_Evasor(25); //25cm=Distancia del obstaculo
+        modo_evasor(25,50); //25cm=Distancia del obstaculo
         break;
 
     case 30:
@@ -99,4 +105,76 @@ char Kit_3_en_1::Leer_BT()
         //Serial.println(Estado);
     }
     return Estado;
+}
+void Kit_3_en_1::modo_evasor(int Distancia, uint8_t velocidad)
+{
+    float Distancia_izq, Distancia_der;
+
+    Motores_mv(velocidad, velocidad);
+    if (obtener_distancia() <= Distancia)
+    {
+        Motores_mv(-50, -50);
+        delay(250);
+        Motores_mv(0, 0);
+
+        //Barrido de servo de derecha a izquierda
+        for (size_t i = 90; i < Angulo_max; i++)
+        {
+            servo_1.write(i);
+            delay(5);
+        }
+
+        delay(300);
+        Distancia_izq = obtener_distancia();
+
+        for (size_t i = Angulo_max; i > Angulo_min; i--)
+        {
+            servo_1.write(i);
+            delay(5);
+        }
+
+        delay(300);
+        Distancia_der = obtener_distancia();
+
+        for (size_t i = Angulo_min; i < 90; i++)
+        {
+            servo_1.write(i);
+            delay(5);
+        }
+        delay(250);
+
+        if (Distancia_izq < Distancia_der)
+        {
+            //Obstaculo detectado en el lado izquierdo, debe girar a la derecha
+            Motores_mv(velocidad_giro, -velocidad_giro);
+            delay(500);
+            Motores_mv(velocidad, velocidad);
+        }
+        else
+        {
+            //Obstaculo detectado en el lado derecho por lo que debe girar a la izquierda
+            Motores_mv(-velocidad_giro, velocidad_giro);
+            delay(500);
+            Motores_mv(velocidad, velocidad);
+        }
+    }
+}
+
+float Kit_3_en_1::obtener_distancia()
+{
+    long lduration;
+    long ldistance;
+
+    //Se manda un pulso por el pin Trigger
+    digitalWrite(Trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trigger, LOW);
+
+    //Se mide cuanto tiempo tardo en us
+    //Ecuación d = l_distancia/2 * velocidad cm/us
+    lduration = pulseIn(Echo, HIGH);
+    ldistance = (lduration / 2) / 29.1;
+    return ldistance;
 }
--- Kit_3_en_1.h
+++ Kit_3_en_1.h
@@ -1,6 +1,6 @@
 #include<Robot.h>
 #include<SoftwareSerial.h>
-
+#include <Servo.h>
 
 class Kit_3_en_1 : public Robot
 {
@@ -14,11 +14,26 @@ private:
     int Velocidad_Max = 230;
     int Velocidad_Med = 130;
 
+    const uint8_t Echo = 11;
+    const uint8_t Trigger = 10;
+    const uint8_t Servo_pin = 13;
+    uint8_t Angulo_min = 0;
+    uint8_t Angulo_max = 180;
+    uint8_t velocidad_giro = 50;
+    Servo servo_1;
+    float distancia;
+
+
 public:
     void init(); 
     void modo_3_en_1();
     char Leer_BT();
     void modo_bluetooth();
+
+    void modo_evasor(int Distancia, uint8_t velocidad);
+    float obtener_distancia();
+    void set_velocidad_giro(uint8_t v_giro);
+
     Kit_3_en_1(/* args */): Robot::Robot(), Bluetooth(B_TX, B_RX){};
     ~Kit_3_en_1();
 };
