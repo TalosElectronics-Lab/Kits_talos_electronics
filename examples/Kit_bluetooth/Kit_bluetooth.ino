a2b089717685f0bc2be994372726dc901cfe8af8
Rafa <rafa.lozano.rolon@gmail.com>
2020-06-16 13:56:43 -0500

    Ajuste kit bt

--- examples/Kit_bluetooth/Kit_bluetooth.ino
+++ examples/Kit_bluetooth/Kit_bluetooth.ino
@@ -1,3 +1,13 @@
+/*
+ *  kit bluetooth 2.0
+ *  Taloselectronics
+ *  soporte@taloselectronics.com Rafael Lozano Rolón
+ * Asegurarse que el kit avance en linea recta con robot_1.Mv_motores(50,50);
+ * calibrar el motor si es necesario de 10 en 10
+ * ejemplo:
+ * robot_1.compensar_motor_derecho(10);
+ * ver vomo se comporta y si es necesario ajustar el los valores en compensar el otro motor.
+ */
 #include<Kit_bluetooth.h>
 Kit_bluetooth Robot;
 void setup()
